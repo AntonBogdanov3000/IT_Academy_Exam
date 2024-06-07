805c7eb65cac0eda3c808d7dfc624c8d5c76f1aa
Anton Bogdanov <folland1@inbox.ru>
2024-06-08 01:42:45 +0300

    changes

--- src/test/java/org/itacademy/browser/BrowserFactory.java
+++ src/test/java/org/itacademy/browser/BrowserFactory.java
@@ -4,8 +4,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 
 public class BrowserFactory {
@@ -14,12 +12,12 @@ public class BrowserFactory {
 
     public static WebDriver createDriver(BrowserType browserType){
         logger.info("Test try runs on browser : " + browserType);
+        WebDriver webDriver = null;
            if (browserType == BrowserType.CHROME) {
                WebDriverManager.chromedriver().setup();
-               return new ChromeDriver();
-           }else
-               return null;
-
-        //logger.debug("Driver " + driver);
+               webDriver = new ChromeDriver();
+           }
+        logger.debug("Driver " + webDriver);
+               return webDriver;
     }
 }
