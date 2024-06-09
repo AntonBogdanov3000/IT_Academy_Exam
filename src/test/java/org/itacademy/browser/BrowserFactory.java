96530c8ed2f942f60def4da732d10778f5812b76
Anton Bogdanov <folland1@inbox.ru>
2024-06-09 20:58:52 +0300

    changes

--- src/test/java/org/itacademy/browser/BrowserFactory.java
+++ src/test/java/org/itacademy/browser/BrowserFactory.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class BrowserFactory {
@@ -15,8 +16,9 @@ public class BrowserFactory {
         WebDriver webDriver = null;
            if (browserType == BrowserType.CHROME) {
                //WebDriverManager.chromedriver().setup();
-               System.setProperty("webdriver.chrome.driver","/Users/Nastya/tools/chromedriver-mac-x64/chromedriver");
-               webDriver = new ChromeDriver();
+               //System.setProperty("webdriver.chrome.driver","/Users/Nastya/tools/chromedriver-mac-x64/chromedriver");
+               System.setProperty("webdriver.gecko.driver", "/Users/Nastya/tools/geckodriver/geckodriver");
+               webDriver = new FirefoxDriver();
            }
         logger.debug("Driver " + webDriver);
                return webDriver;
