b467f595caf9b9f029a9ef8740eede87159feb44
Anton Bogdanov <folland1@inbox.ru>
2024-06-17 21:05:00 +0300

    add changes

---
+++ standalone.cmd
@@ -0,0 +1,1 @@
+java -Dwebdriver.chrome.driver=/Users/Nastya/tools/chromedriver-mac-x64/chromedriver -jar /Users/Nastya/tools/selenium-server-4.21.0/selenium-server-4.21.0.jar standalone --port 4444
